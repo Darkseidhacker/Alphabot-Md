2b83460e55cde198c14b88300712fe706a128a4e
Darkseidhacker <105297962+Darkseidhacker@users.noreply.github.com>
2022-07-28 12:22:22 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -19,28 +19,28 @@ global.autobio = true
 //language
 //Available in indonesia , english & spanyol 
 //You can request others language 
-global.language = indonesia  //change indonesia to english if you don't understand the language used by the bot
+global.language = english  //change indonesia to english if you don't understand the language used by the bot
 
 
 // Other
 // Other
 global.botname = "Alphabot-Mdོ" //namabot kalian
-global.ownername= "ᴹᴿ᭄ ZeeoneOfcོ ×፝֟͜×" //nama kalian
+global.ownername= "ᴹᴿ᭄ Darkseid ×፝֟͜×" //nama kalian
 global.myweb ="https://api.zeeoneofc.xyz" //bebas asal jan hapus
-global.youtube = "https://youtube.com/c/ZeeoneOfc" //bebas asal jan hapus
+global.youtube = "https://www.youtube.com/channel/UCfjMNznC1wPPnzZTXvdAKpQ" //bebas asal jan hapus
 global.github = "https://zeeone-ofc.github.io/" //bebas
-global.email = "zeeoneofc@gmail.com" //bebas
-global.region = "Indonesia" //bebas
-global.timezone = 'Asia/Jakarta' //  timezone wib
-global.premium = ['62887435047326'] //premium user
-global.owner = ["622150996855","6285342106390"] //ganti agar fitur owner bisa di gunakan
-global.ownernomer = "62887435047326" // nomor wa kalian
-global.ownernomerr = "+62887435047326" //nmr wa kalian
+global.email = "naisond94@gmail.com" //bebas
+global.region = "Zimbabwe" //bebas
+global.timezone = 'Africa/Harare' //  timezone wib
+global.premium = ['263718347537'] //premium user
+global.owner = ["263718347537","14314005671"] //ganti agar fitur owner bisa di gunakan
+global.ownernomer = "263718347537" // nomor wa kalian
+global.ownernomerr = "+263718347537" //nmr wa kalian
 global.thumbnail = "./image/lol.jpg" // ini lol.jpg adalah nama foto di folder image. untuk foto bot
 global.donasi = "./image/donasi.jpg" // foto donasi di folder image
 global.background_welcome="https://telegra.ph/file/90a931648de597820bc08.jpg" // maks size 30kb, agar welcome image nya tdk delay
 global.packname = '© Alphabot-Mdོ' //sticker wm ubah
-global.author = 'Di Buat Oleh ZeeoneOfc' //sticker wm ganti nama kalian
+global.author = 'Creator Mr Darkseid' //sticker wm ganti nama kalian
 global.sessionName = 'session'
 global.typemenu = 'document'
 
